104bfa81a3b573f0d31fdef5bc47e97ad2db82e3
Stew <anast.bd@yandex.ru>
2020-05-21 06:39:35 +0000

    initial commit

---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Profile = () => {
+	return (
+		<div className='content'>
+			<div>
+			<img src='https://images.unsplash.com/photo-1589911830828-964ec79fa47b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80'/>
+			</div>
+			<div>
+				ava + desctiption
+			</div>
+			<div>
+				My posts
+				<div>
+					New post
+				</div>
+				<div>
+					Post 1
+				</div>
+				<div>
+					Post 2
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default Profile;
