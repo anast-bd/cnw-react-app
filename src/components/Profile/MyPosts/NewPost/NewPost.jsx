b01004306ee3494482041a82ebee056078794eba
anast-bd <anast.bd@yandex.ru>
2020-05-28 13:24:05 +0000

    add button

--- src/components/Profile/MyPosts/NewPost/NewPost.jsx
+++ src/components/Profile/MyPosts/NewPost/NewPost.jsx
@@ -9,6 +9,7 @@ const NewPost = () => {
 				<img src='https://images.unsplash.com/photo-1496070242169-b672c576566b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=641&q=80' />
 				<input placeholder='Write something here...'>
 				</input>
+				<button className='postButton'>Post</button>
 			</div>
 		</div>
 	)
