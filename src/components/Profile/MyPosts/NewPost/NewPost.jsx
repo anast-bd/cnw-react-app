11dac43f4950e1e89f287ad697df44fda97cd6c4
anast-bd <anast.bd@yandex.ru>
2020-05-28 14:34:52 +0000

    add listener on newPost button

--- src/components/Profile/MyPosts/NewPost/NewPost.jsx
+++ src/components/Profile/MyPosts/NewPost/NewPost.jsx
@@ -2,14 +2,21 @@ import React from 'react';
 import s from './NewPost.module.css';
 
 const NewPost = () => {
+	
+	let newPostElement = React.createRef();
+	
+	let addPost = () => { 
+		let text = newPostElement.current.value;
+		alert(text) }
+	
 	return (
 		<div className={s.newPostContainer}>
 			<div className={s.cardHeader}>New post</div>
 			<div className={s.cardBody}>
 				<img src='https://images.unsplash.com/photo-1496070242169-b672c576566b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=641&q=80' />
-				<input placeholder='Write something here...'>
+				<input ref={newPostElement} placeholder='Write something here...'>
 				</input>
-				<button className='postButton'>Post</button>
+				<button className='postButton' onClick={ addPost }>Post</button>
 			</div>
 		</div>
 	)
