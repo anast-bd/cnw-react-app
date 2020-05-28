85274a155105a61965fc723ad98d666643fa4595
anast-bd <anast.bd@yandex.ru>
2020-05-28 19:30:44 +0000

    add new message text area to dialogs [iss31]

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -9,6 +9,14 @@ const Dialogs = (props) => {
 	let dialogsElements = props.state.dialogs.map (d => <DialogItem name={d.name} id={d.id} image={d.image} />);
 	let messagesElements = props.state.messages.map (m => <Message message={m.message} />);
 	
+	let newMessageElement = React.createRef();
+	
+	let sendMessage = () => {
+		let text = newMessageElement.current.value;
+		alert(text);
+	}
+	
+	
 	return (
 		<div className={s.dialogs}>
 			
@@ -16,8 +24,19 @@ const Dialogs = (props) => {
 				{ dialogsElements } 
 			</div>
 			
-			<div className={s.messages}>
-				{ messagesElements }
+			<div className={s.chat}>
+				<div className={s.chatHeader}>
+					{props.state.dialogs[0].name}
+				</div>
+				<div className={s.messages}>
+					{ messagesElements }
+				</div>
+				<div className={s.chatFooter}>
+					<textarea ref={newMessageElement} placeholder='Enter your message ...'></textarea>
+					<div>
+						<button onClick={sendMessage}>Send</button> 
+					</div>
+				</div>
 			</div>
 
 		</div>
