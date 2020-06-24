19042c1f8151f4c44cfdf1f24d9fd6fa732b2954
anast-bd <anast-bd@yandex.com>
2020-06-24 15:38:30 +0700

    use componentDidUpdate method, add Users link on sidebar [iss54]

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -6,13 +6,11 @@ import userPhoto from '../../assets/images/default-user-img.png';
 
 class Users extends React.Component {
 
-	constructor(props) {
-		super(props);
+	componentDidMount() {
 		axios.get('https://social-network.samuraijs.com/api/1.0/users')
 		.then (response => {
 			this.props.setUsers(response.data.items);
 		})
-
 	}
 
 	render() {
